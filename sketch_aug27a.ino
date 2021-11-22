d5f4ba67c92f4c899f4103ba0383043b1367b0e5
TintedFireglass <tintedfireglass@hotmail.com>
2021-11-22 23:02:22 +0530

    Add files via upload

---
+++ sketch_aug27a.ino
@@ -0,0 +1,25 @@
+void setup()
+{
+  pinMode(A0,INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+ int a= analogRead(A0);
+ int b= map(a,0,600,0,2);
+  switch(b)
+  { 
+  case 0:
+  Serial.println("dark");
+  break;
+  
+  case 1:
+  Serial.println("medium");
+  break;
+  
+  case 2:
+  Serial.println("bright");
+  break;
+  }
+}
